3d378be662f1f6f90e0f4e73ee9e2e4c15579ba7
michaelkiundi <michaelkiundi@gmail.com>
2022-09-19 10:52:41 -0700

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,8 +10,14 @@ void puts_half(char *str)
 
 	while (*str != '\0')
 	{
-		_putchar(*str);
+		len++;
 		str++;
+	}
+	str -= (len / 2);
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str;
 	}
 	_putchar('\n');
 }
