7358b3ce3c58550af9741a4301fbc1247ea1a8df
michaelkiundi <michaelkiundi@gmail.com>
2022-09-19 11:51:28 -0700

    task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -8,12 +8,9 @@
  */
 void _puts(char *str)
 {
-	int i = 0;
-
-	while (*(str + i) != '\0')
+	for (; *str != '\0'; str++)
 	{
-		putchar(*(str + i));
-		i++;
+		_putchar(*str);
 	}
-	putchar(10);
+	_putchar('\n');
 }
