7caad1a40f5beea435b0ad12075ef2761ce07d3b
michaelkiundi <michaelkiundi@gmail.com>
2022-09-19 05:36:24 -0700

    task 0

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,11 +4,10 @@
  * void prototypes()
  * int prototypes()
  */
-int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-int _strlen(char *str);
-void _puts(char *s);
+int _strlen(char *s);
+void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
