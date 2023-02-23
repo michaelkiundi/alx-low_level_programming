23aca666e0965893133cb5d6d3ed0ccf5f93c3c5
Michael Kiundi <michaelkiundi@gmail.com>
2023-02-23 09:23:12 -0800

    task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -22,13 +22,12 @@ int largest_number(int a, int b, int c)
 	}
 	else if (b > c)
 	{
-		largest = b;
+	largest = b;
 	}
 	else
 	{
-		largest = c;
+	largest = c;
 	}
 
-return (largest);
+	return (largest);
 }
-
